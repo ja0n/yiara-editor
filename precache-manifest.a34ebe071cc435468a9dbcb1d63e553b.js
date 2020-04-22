9a96053de5fb67d89257d6d8a72f786c91596cb7
Jaon <jamonsterr@gmail.com>
2020-04-22 02:22:54 -0300

    Updates

---
+++ precache-manifest.a34ebe071cc435468a9dbcb1d63e553b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7a6959cf44715ec7a9edb50f891175",
+    "url": "/yiara-editor/index.html"
+  },
+  {
+    "revision": "9965eaa51c955a215d19",
+    "url": "/yiara-editor/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "7fba8fc8290cbff65247",
+    "url": "/yiara-editor/static/js/2.7d1fd2f6.chunk.js"
+  },
+  {
+    "revision": "4f69b4f29e20ea9f689941faf6a46e34",
+    "url": "/yiara-editor/static/js/2.7d1fd2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9965eaa51c955a215d19",
+    "url": "/yiara-editor/static/js/main.d64fa6ce.chunk.js"
+  },
+  {
+    "revision": "2ab11bc075728d21e819",
+    "url": "/yiara-editor/static/js/runtime-main.bede2f38.js"
+  }
+]);
